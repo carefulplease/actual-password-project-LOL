ef5d927b028f8e9a7e48f889b6620921343e91d1
prebuiltpc <prebuiltpc@protonmail.com>
2020-05-29 17:28:13 -0700

    hi

---
+++ actual password project LOL/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace actual_password_project_LOL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.poop = new System.Windows.Forms.Button();
+            this.ball = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.exit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // poop
+            // 
+            this.poop.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.poop.Font = new System.Drawing.Font("Franklin Gothic Book", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.poop.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.poop.Location = new System.Drawing.Point(57, 342);
+            this.poop.Name = "poop";
+            this.poop.Size = new System.Drawing.Size(168, 52);
+            this.poop.TabIndex = 0;
+            this.poop.Text = "generate";
+            this.poop.UseVisualStyleBackColor = false;
+            this.poop.Click += new System.EventHandler(this.poop_Click);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.White;
+            this.ball.Font = new System.Drawing.Font("Franklin Gothic Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ball.ForeColor = System.Drawing.Color.Black;
+            this.ball.Location = new System.Drawing.Point(57, 248);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(168, 26);
+            this.ball.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::actual_password_project_LOL.Properties.Resources.pass;
+            this.panel1.Controls.Add(this.exit);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(288, 180);
+            this.panel1.TabIndex = 1;
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.Color.DarkRed;
+            this.exit.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.exit.FlatAppearance.BorderSize = 0;
+            this.exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit.Location = new System.Drawing.Point(246, 0);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(42, 31);
+            this.exit.TabIndex = 1;
+            this.exit.Text = "X";
+            this.exit.UseVisualStyleBackColor = false;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::actual_password_project_LOL.Properties.Resources.tomato;
+            this.pictureBox1.Location = new System.Drawing.Point(34, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(213, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 450);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.poop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button poop;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox ball;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button exit;
+    }
+}
+
---
+++ actual password project LOL/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace actual_password_project_LOL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void poop_Click(object sender, EventArgs e)
+        {
+            //this is the minimum length and maximum length 
+            int minLength = 8;
+            int maxLength = 16;
+            //characters that have an opprotunity to show up in gen
+            string charAvailable = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()_+-=[];',./";
+
+            StringBuilder password = new StringBuilder();
+            Random rdm = new Random();
+
+            
+            int passwordLength = rdm.Next(minLength, maxLength + 1);
+            
+            while (passwordLength-- > 0)
+                password.Append(charAvailable[rdm.Next(charAvailable.Length)]);
+
+            ball.Text = password.ToString();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+
+        }
+        Point lastPoint;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
